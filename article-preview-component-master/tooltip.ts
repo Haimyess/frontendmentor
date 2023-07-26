039bc41283184bb5a2c9c0bd8cef69a60973d901
Haim Yeshurun <haimyess@gmail.com>
2023-07-26 22:09:04 +0300

    Tooltip functionality

--- article-preview-component-master/tooltip.ts
+++ article-preview-component-master/tooltip.ts
@@ -38,14 +38,26 @@ function createButton() {
 console.log('hi')
 // The author class we will change the content
 
+// let isShow = false;
 
 const shareBtn = document.getElementById('share')
 console.log(shareBtn)
+shareBtn?.addEventListener('click', share)
 // Button functionality
 
 function share() {
+    // isShow = true;
 
     // select author
+    const authorSection = document.getElementById('author')
+    authorSection?.classList.toggle('hide')
 
-    // show tooltip mobile 
+    const tooltip = document.getElementById('tooltip')
+    tooltip?.classList.toggle('hide')
+
+    const shareContainer = document.querySelector('.share')
+    shareContainer?.classList.toggle('active')
+
+    // if(isShow) {}
+   
 }
--- dest/article-preview-component-master/tooltip.js
+++ dest/article-preview-component-master/tooltip.js
@@ -19,10 +19,19 @@ function createButton() {
 }
 console.log('hi');
 // The author class we will change the content
+// let isShow = false;
 const shareBtn = document.getElementById('share');
 console.log(shareBtn);
+shareBtn === null || shareBtn === void 0 ? void 0 : shareBtn.addEventListener('click', share);
 // Button functionality
 function share() {
+    // isShow = true;
     // select author
-    // show tooltip mobile 
+    const authorSection = document.getElementById('author');
+    authorSection === null || authorSection === void 0 ? void 0 : authorSection.classList.toggle('hide');
+    const tooltip = document.getElementById('tooltip');
+    tooltip === null || tooltip === void 0 ? void 0 : tooltip.classList.toggle('hide');
+    const shareContainer = document.querySelector('.share');
+    shareContainer === null || shareContainer === void 0 ? void 0 : shareContainer.classList.toggle('active');
+    // if(isShow) {}
 }
